e8f0d535370c7a2891044fb7da2a1db0adea89f9
Bipin Gupta <bipinmanoj.gupta@student.griffith.ie>
2025-04-08 04:31:52 +0100

    added user login methof]d

--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../modules/User');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken'); 
-const user_jwt = require('../middleware/user_jwt');
+const User = require("../modules/User");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const user_jwt = require("../middleware/user_jwt");
 
-router.get('/', user_jwt, async(req, res, next) => {
-    try{
-        const user = await User.findById(req.user.id).select('-password');
-        res.status(200).json({
-            success : true,
-            user : user
-        });
-    }
-    catch(err){
-        console.log(err.message);
-        res.status(500).json({
-            success : false,
-            msg: 'server error'
-        })
-        next();
-    }
-})
+router.get("/", user_jwt, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.status(200).json({
+      success: true,
+      user: user,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({
+      success: false,
+      msg: "server error",
+    });
+    next();
+  }
+});
 
-router.post('/register', async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   const { username, email, password } = req.body;
 
   try {
@@ -32,7 +31,7 @@ router.post('/register', async (req, res, next) => {
     if (user_exist) {
       return res.json({
         success: false,
-        msg: 'user already exists',
+        msg: "user already exists",
       });
     }
 
@@ -70,7 +69,61 @@ router.post('/register', async (req, res, next) => {
     console.error(err.message);
     res.status(500).json({
       success: false,
-      msg: 'Server Error',
+      msg: "Server Error",
+    });
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    let user = await User.findOne({
+      email: email,
+    });
+
+    if (!user) {
+      res.status(400).json({
+        success: false,
+        msg: "User not exists",
+      });
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({
+        success: false,
+        msg: "Password incorrect",
+      });
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(payload, process.env.jwtUserSecret, {
+        expiresIn: 360000
+    }, (error, token) => {
+        if(error) throw error;
+
+        res.status(200).json({
+            success : true, 
+            msg : "user logged in",
+            token : token,
+            user: user
+        })
+    })
+
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server error",
     });
   }
 });
